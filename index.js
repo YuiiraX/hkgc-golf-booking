601e5aecd4de41fb1068ff08d1c7c7b40cde219d
Ira Chan <ira@cga.gg>
2021-06-08 08:23:35 +0800

    Add timer for automatic trigger

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { timetable } = require("./timetable.json");
 const open = require("open");
 
-const start = 12;
-const end = 16;
-const slots = timetable.NewCourse.Slots;
+const start = 8;
+const end = 12;
+const slots = timetable.EdenCourse.Slots;
 const baseUrl = "https://visitorbookings.hkgolfclub.org/Booking/AddBooking/";
 const suffix = "?fm=bv";
 
@@ -15,8 +15,23 @@ const morningSlots = slots.filter((item) => {
   );
 });
 
-console.log(morningSlots.length);
-morningSlots.forEach(async (slot) => {
-  console.log("Timeslot: ", slot.Timeslot, slot.WholeTimeslot);
-  await open(baseUrl + slot.WholeTimeslot + suffix);
-});
+// console.log(morningSlots.length);
+function openBooking(slots) {
+  slots.forEach(async (slot) => {
+    console.log("Timeslot: ", slot.Timeslot, slot.WholeTimeslot);
+    await open(baseUrl + slot.WholeTimeslot + suffix);
+  });
+}
+
+var interval = setInterval(() => {
+  var now = new Date()
+  var target = new Date()
+  target.setHours(12)
+  target.setMinutes(13)
+  target.setSeconds(0)
+  console.log(now, target)
+  if(now > target) {
+    openBooking(morningSlots)
+    clearInterval(interval)
+  }
+}, 1000);
